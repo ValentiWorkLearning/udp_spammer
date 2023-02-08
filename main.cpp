a77e7f578207ee1551cec3568f3d7b88f30c441c
Valentyn Korniienko <kornienko-vr@rambler.ru>
2023-02-08 18:03:34 +0200

    [cmdargs] added port argument

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 #include <fstream>
 #include <charconv>
 #include <cctype>
-
+#include <string_view>
+#include <boost/lexical_cast.hpp>
 
 using boost::asio::ip::udp;
 using boost::asio::ip::address;
@@ -17,8 +18,10 @@ namespace {
 
 	class UdpSpammerServer {
 	public:
-		UdpSpammerServer(boost::asio::io_service& io_service, TUDPDataPtr&& messageToSpam)
+		UdpSpammerServer(boost::asio::io_service& io_service,std::string_view ipAddress,std::uint16_t port,TUDPDataPtr&& messageToSpam)
 			: m_socket(io_service)
+			, m_ipAddress{ipAddress}
+			, m_port{port}
 			, m_spamMessage{ std::move(messageToSpam) }
 		{
 			m_socket.open(udp::v4());
@@ -31,7 +34,7 @@ namespace {
 	private:
 
 		void spamMessage() {
-				udp::endpoint remoteEndpoint{ udp::endpoint{address::from_string("127.0.0.1"),5555} };
+				udp::endpoint remoteEndpoint{ udp::endpoint{address::from_string(m_ipAddress.data()),m_port} };
 				m_socket.async_send_to(boost::asio::buffer(*m_spamMessage), remoteEndpoint,
 					
 					boost::bind(&UdpSpammerServer::sendReady, this, m_spamMessage,
@@ -46,8 +49,10 @@ namespace {
 		{
 			spamMessage();
 		}
-		TUDPDataPtr m_spamMessage;
 		udp::socket m_socket;
+		std::uint16_t m_port;
+		std::string_view m_ipAddress;
+		TUDPDataPtr m_spamMessage;
 	};
 
 }  // namespace
@@ -93,9 +98,17 @@ TUDPDataPtr readSpamData(const std::filesystem::path& filePathData)
 int main(int argc, char* argv[]) {
 	try {
 		boost::asio::io_service io_service;
-		auto pSpamData{ readSpamData(std::filesystem::path(argv[1])) };
 
-		UdpSpammerServer server{ io_service,std::move(pSpamData) };
+		auto ipAddress{std::string_view{argv[1]}};
+		auto port {std::string_view{argv[2]}};
+		auto pSpamData{ readSpamData(std::filesystem::path(argv[3])) };
+
+		UdpSpammerServer server{
+			io_service,ipAddress,
+			boost::lexical_cast<std::uint16_t>(port),
+			std::move(pSpamData)
+		};
+
 		server.startSpamming();
 		io_service.run();
 	}
